b9581edecf3002ed72f1b8cbf3fc5646e13fb1eb
Nicholas Fransisco <nicholasfransisco09@gmail.com>
2025-07-23 15:32:15 +0700

    feat: aos once

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -140,18 +140,18 @@ const HomePage = () => {
             <div className="w-full h-screen flex items-center justify-center bg-white px-30 max-[1280px]:px-20 max-md:px-10 max-md:py-10 max-[991px]:h-auto max-[991px]:mt-[70px] max-[991px]:py-20 max-sm:px-5">
                 <div className="flex max-[991px]:flex-col-reverse justify-between items-center w-full gap-10">
                     <div className="w-7/12 max-[991px]:w-full mb-10 md:mb-0 max-sm:px-5">
-                        <h1 className="text-5xl max-sm:text-4xl font-bold text-black mb-6 max-sm:text-center leading-[60px] max-sm:leading-[50px]" data-aos="fade-right">
+                        <h1 className="text-5xl max-sm:text-4xl font-bold text-black mb-6 max-sm:text-center leading-[60px] max-sm:leading-[50px]" data-aos="fade-right" data-aos-once="true">
                             lapor<span className="text-red-600">PAK</span> by <span className="text-red-600">City Department®</span>
                         </h1>
-                        <p className="text-gray-700 mb-8 leading-relaxed max-sm:text-center" data-aos="fade-right">
+                        <p className="text-gray-700 mb-8 leading-relaxed max-sm:text-center" data-aos="fade-right" data-aos-once="true">
                             Punya keluhan soal lingkungan kota? Sampaikan langsung di LaporPAK! Kami adalah platform pengaduan terpadu yang siap menjembatani suara Anda dengan pihak berwenang. Bersama, kita wujudkan perubahan.
                         </p>
 
                         <hr className="mb-8 border-gray-300" />
 
-                        <h2 className="text-2xl font-semibold mb-5 max-sm:text-center" data-aos="fade-up">Punya Keluhan atau Masukan?</h2>
+                        <h2 className="text-2xl font-semibold mb-5 max-sm:text-center" data-aos="fade-up" data-aos-once="true">Punya Keluhan atau Masukan?</h2>
 
-                        <div className="flex max-md:flex-col gap-4 max-sm:justify-center w-full" data-aos="fade-up">
+                        <div className="flex max-md:flex-col gap-4 max-sm:justify-center w-full" data-aos="fade-up" data-aos-once="true">
                             <Link to="/">
                                 <button className="cursor-pointer px-6 py-3 border-2 border-red-600 text-red-600 font-semibold rounded-none hover:bg-red-600 hover:text-white transition max-md:w-full">
                                     Registrasi akun
@@ -166,7 +166,7 @@ const HomePage = () => {
                     </div>
 
                     <div className="w-5/12 max-[991px]:w-full flex justify-center relative">
-                        <div className="relative w-96 h-96 max-lg:w-72 rounded-full max-lg:h-72 overflow-hidden" data-aos="fade-left">
+                        <div className="relative w-96 h-96 max-lg:w-72 rounded-full max-lg:h-72 overflow-hidden" data-aos="fade-left" data-aos-once="true">
                             <img
                                 src="/images/mascot.png"
                                 alt="User using phone"
@@ -189,45 +189,45 @@ const HomePage = () => {
             </div>
 
             <div className="bg-red-50 w-full px-30 max-[1280px]:px-20 py-20 max-md:px-10 max-md:py-10 flex flex-col items-center justify-center">
-                <h1 className="text-5xl font-bold text-black text-center mb-15 max-sm:text-4xl max-sm:mb-10" data-aos="fade-down">Cara Membuat Laporan</h1>
+                <h1 className="text-5xl font-bold text-black text-center mb-15 max-sm:text-4xl max-sm:mb-10" data-aos="fade-down" data-aos-once="true">Cara Membuat Laporan</h1>
                 <div className="w-full grid grid-cols-3 gap-5 max-[991px]:grid-cols-1">
                     <div className="flex flex-col items-center">
-                        <div className="w-60 h-60 xl:w-72 xl:h-72 rounded-full overflow-hidden mx-auto border-6 border-rose-200 shadow-lg mb-2 hover:scale-105 transition duration-300" data-aos="zoom-in">
+                        <div className="w-60 h-60 xl:w-72 xl:h-72 rounded-full overflow-hidden mx-auto border-6 border-rose-200 shadow-lg mb-2 hover:scale-105 transition duration-300" data-aos="zoom-in" data-aos-once="true">
                             <img
                                 src="/images/step1.png"
                                 alt="Step 1"
                                 className="object-cover w-full h-full"
                             />
                         </div>
-                        <div className="w-60 max-[991px]:w-2/4 max-sm:w-full flex flex-col justify-center text-center items-center" data-aos="fade-up">
+                        <div className="w-60 max-[991px]:w-2/4 max-sm:w-full flex flex-col justify-center text-center items-center" data-aos="fade-up" data-aos-once="true">
                             <h2 className="text-xl font-semibold mt-4 rounded-full bg-red-700 pb-1 text-white w-8 h-8 flex items-center justify-center">1</h2>
                             <h2 className="text-xl font-semibold mt-4 mb-2">Registrasi / Login</h2>
                             <p className="text-gray-700">Buat akun atau masuk untuk melacak dan mengelola laporan Anda dengan mudah</p>
                         </div>
                     </div>
                     <div className="flex flex-col items-center">
-                        <div className="w-60 h-60 xl:w-72 xl:h-72 rounded-full overflow-hidden mx-auto border-6 border-rose-200 shadow-lg mb-2 hover:scale-105 transition duration-300" data-aos="zoom-in">
+                        <div className="w-60 h-60 xl:w-72 xl:h-72 rounded-full overflow-hidden mx-auto border-6 border-rose-200 shadow-lg mb-2 hover:scale-105 transition duration-300" data-aos="zoom-in" data-aos-once="true">
                             <img
                                 src="/images/step2.png"
                                 alt="Step 2"
                                 className="object-cover w-full h-full"
                             />
                         </div>
-                        <div className="w-60 max-[991px]:w-2/4 max-sm:w-full flex flex-col justify-center text-center items-center" data-aos="fade-up">
+                        <div className="w-60 max-[991px]:w-2/4 max-sm:w-full flex flex-col justify-center text-center items-center" data-aos="fade-up" data-aos-once="true">
                             <h2 className="text-xl font-semibold mt-4 rounded-full bg-red-700 pb-1 text-white w-8 h-8 flex items-center justify-center">2</h2>
                             <h2 className="text-xl font-semibold mt-4 mb-2">Laporkan Masalah</h2>
                             <p className="text-gray-700">Jelaskan keluhan Anda dengan jelas, tambahkan foto atau detail lokasi, lalu kirim laporan Anda</p>
                         </div>
                     </div>
                     <div className="flex flex-col items-center">
-                        <div className="w-60 h-60 xl:w-72 xl:h-72 rounded-full overflow-hidden mx-auto border-6 border-rose-200 shadow-lg mb-2 hover:scale-105 transition duration-300" data-aos="zoom-in">
+                        <div className="w-60 h-60 xl:w-72 xl:h-72 rounded-full overflow-hidden mx-auto border-6 border-rose-200 shadow-lg mb-2 hover:scale-105 transition duration-300" data-aos="zoom-in" data-aos-once="true">
                             <img
                                 src="/images/step3.png"
                                 alt="Step 3"
                                 className="object-cover w-full h-full"
                             />
                         </div>
-                        <div className="w-60 max-[991px]:w-2/4 max-sm:w-full flex flex-col justify-center text-center items-center" data-aos="fade-up">
+                        <div className="w-60 max-[991px]:w-2/4 max-sm:w-full flex flex-col justify-center text-center items-center" data-aos="fade-up" data-aos-once="true">
                             <h2 className="text-xl font-semibold mt-4 rounded-full bg-red-700 pb-1 text-white w-8 h-8 flex items-center justify-center">3</h2>
                             <h2 className="text-xl font-semibold mt-4 mb-2">Diskusi & Kolaborasi</h2>
                             <p className="text-gray-700">Lihat laporan dari pengguna lain, beri komentar, upvote, dan bekerja sama untuk menyuarakan masalah</p>
@@ -247,15 +247,15 @@ const HomePage = () => {
             {/* Top Vote Section */}
             <div className="w-full px-30 max-[1280px]:px-20 py-20 max-md:px-10 max-sm:px-5 max-md:py-10">
                 <div className="flex justify-between items-center mb-15 max-sm:mb-10 max-sm:flex-col">
-                    <h1 className="text-5xl font-bold text-black max-sm:text-4xl max-sm:mb-5 max-sm:text-center pb-2" data-aos='fade-right'>Laporan Teratas</h1>
-                    <Link to="/report" data-aos='fade-left'>
+                    <h1 className="text-5xl font-bold text-black max-sm:text-4xl max-sm:mb-5 max-sm:text-center pb-2" data-aos='fade-right' data-aos-once="true">Laporan Teratas</h1>
+                    <Link to="/report" data-aos='fade-left' data-aos-once="true">
                         <button className="cursor-pointer px-6 py-2 border-2 border-red-600 text-red-600 font-semibold rounded-none hover:bg-red-600 hover:text-white transition max-md:w-full">
                             Lihat Semua
                         </button>
                     </Link>
                 </div>
                 <div className="flex justify-center items-center">
-                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5" data-aos="fade-up">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5" data-aos="fade-up" data-aos-once="true">
                         {reports.map((report, index) => (
                             <ReportCard
                                 key={index}
@@ -276,12 +276,12 @@ const HomePage = () => {
             </div>
 
             <div className="w-full px-6 py-10 flex flex-col items-center justify-center bg-red-50">
-                <h1 className="text-5xl font-bold text-black text-center mb-10 max-sm:text-4xl max-sm:mb-6" data-aos="fade-down">
+                <h1 className="text-5xl font-bold text-black text-center mb-10 max-sm:text-4xl max-sm:mb-6" data-aos="fade-down" data-aos-once="true">
                     Ringkasan Laporan
                 </h1>
 
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full max-w-7xl">
-                    <div className="bg-white rounded-2xl shadow-md p-6 w-full" data-aos="slide-up">
+                    <div className="bg-white rounded-2xl shadow-md p-6 w-full" data-aos="slide-up" data-aos-once="true">
                         <h2 className="text-xl md:text-2xl font-bold text-black text-center mb-4 md:mb-5 lg:mb-6">
                             Laporan berdasarkan kategori
                         </h2>
@@ -313,7 +313,7 @@ const HomePage = () => {
                     </div>
 
                     {/* Card 2 */}
-                    <div className="bg-white rounded-2xl shadow-md p-6 w-full" data-aos="slide-up" ref={chartContainerRef}>
+                    <div className="bg-white rounded-2xl shadow-md p-6 w-full" data-aos="slide-up" data-aos-once="true" ref={chartContainerRef}>
                         <h2 className="text-xl md:text-2xl font-bold text-black text-center mb-4 md:mb-5 lg:mb-6">
                             Jumlah Laporan per Tahun
                         </h2>
