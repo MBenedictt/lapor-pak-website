8dfb72907832fb9860999f2c10ffee9ef92f1c3f
MBThePro <131933737+MBThePro@users.noreply.github.com>
2025-07-23 16:18:51 +0700

    fix: update login link in Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -109,7 +109,7 @@ const Navbar = () => {
                             </Link>
                         </li>
                         <li className="font-medium text-2xl py-4">
-                            <Link to="/" className="relative group flex w-fit">
+                            <Link to="/login" className="relative group flex w-fit">
                                 <span className="group-hover:after:w-full after:w-0 after:h-0.5 after:bg-black after:absolute after:bottom-0 after:left-0 after:transition-all after:duration-300">
                                     Login
                                 </span>
